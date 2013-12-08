99d26f4174bcc22b417fd86f80cde05646a2d370
Josh Aronoff <aronoff@gmail.com>
2013-12-08 14:35:09 -0500

    changed about page copy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,64 +1,79 @@
 require 'bundler/capistrano'
-require 'rvm/capistrano'
-
-#App Settings
+ 
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ 
+set :user, "jaronoff"
 set :application, 'Josh_New_Site'
 set :repository, 'git@example.com:me/my_repo.git'
-
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-set :deploy_to, '/home/jaronoff/webapps/joshsite'
+set :deploy_to, "/home/jaronoff/webapps/joshsite"
+set :default_stage,"production"
+set :assets_path, "#{deploy_to}/shared/assets"
+set :verbose_command_log, true
+ 
+set :use_sudo, false
+set :deploy_via, :checkout
+set :branch, "master"
+ 
+set :default_environment, {
+  'PATH' => "#{deploy_to}/bin:$PATH",
+  'GEM_HOME' => "#{deploy_to}/gems",
+  'RAILS_ENV' => "#{default_stage}"
+}
+ 
 set :scm, :git
-
-#Server Roles
 role :web, "web432.webfaction.com"
 role :app, "web432.webfaction.com"
-role :db,  "web432.webfaction.com", :primary => true
-
-#User Settings
-set :user, "jaronoff"
-set :scm_username, "jaronoff"
-set :use_sudo, false
-default_run_options[:pty] = true
-
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
-
+role :db,  "web432.webfaction.com", :primary => true 
+ 
+desc "Restart nginx"
+task :restart do
+  run "#{deploy_to}/bin/restart"
+end
+ 
+desc "Start nginx"
+task :start do
+  run "#{deploy_to}/bin/start"
+end
+ 
+desc "Stop nginx"
+task :stop do
+  run "#{deploy_to}/bin/stop"
+end
+ 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+  
+  puts "============================================="
+  puts "SIT BACK AND RELAX WHILE CAPISTRANO ROCKS ON!"
+  puts "============================================="
+  
+  desc "Seed database"
+  task :remakedb do
+    run "cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=#{default_stage}"
+    run "cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=#{default_stage}"
+  end
+  
+  desc "Seed database"
+  task :seed do
+    run "cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=#{default_stage}"
+  end
+  
+  desc "Migrate database"
+  task :migrate do
+    run "cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=#{default_stage}"
+  end
+ 
+  namespace :assets do
+    desc 'Run the precompile task locally and rsync with shared'
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      %x{bundle exec rake assets:precompile}
+      %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{domain}:#{assets_path}}
+      %x{bundle exec rake assets:clean}
     end
   end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  namespace :deploy do
-  desc "Restart nginx"
-  task :restart do
-    run "#{deploy_to}/bin/restart"
-  end
+  
 end
-
-  after :finishing, 'deploy:cleanup'
-
-
-end
+ 
+after "deploy", "assets:precompile"
+after "deploy", "deploy:migrate"
+after "deploy", "restart"
---
+++ config/deploy2.rb
@@ -0,0 +1,65 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+#App Settings
+set :application, 'Josh_New_Site'
+set :repository, 'git@example.com:me/my_repo.git'
+set :branch, "master"
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/home/jaronoff/webapps/joshsite'
+set :scm, :git
+
+#Server Roles
+role :web, "web432.webfaction.com"
+role :app, "web432.webfaction.com"
+role :db,  "web432.webfaction.com", :primary => true
+
+#User Settings
+set :user, "jaronoff"
+set :scm_username, "jaronoff"
+set :use_sudo, false
+default_run_options[:pty] = true
+
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  namespace :deploy do
+  desc "Restart nginx"
+  task :restart do
+    run "#{deploy_to}/bin/restart"
+  end
+end
+
+  after :finishing, 'deploy:cleanup'
+
+
+end
