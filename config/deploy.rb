b98be0b966d72bb0331fa415e4cdf4a49a0cdb67
Josh Aronoff <aronoff@gmail.com>
2014-01-07 21:44:05 -0500

    Made a projects page, may be a duplicate.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :branch, "master"
 
 set :scm, :git
 set :deploy_to, '/home/jaronoff/webapps/portfolio_site/'
-
+set :keep_releases, 5
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
